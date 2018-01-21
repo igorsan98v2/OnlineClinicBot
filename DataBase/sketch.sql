11298dbe7492f9e0b7c82efbcf09b300a97a4c4f
igorsan98v2 <34813105+igorsan98v2@users.noreply.github.com>
2018-01-21 22:34:38 +0200

    Add files via upload

--- DataBase/sketch.sql
+++ DataBase/sketch.sql
@@ -1,30 +1,36 @@
 CREATE TABLE doctors(
-id INT PRIMARY KEY AUTOINCREMENT,
+id INTEGER PRIMARY KEY AUTOINCREMENT,
 first_name TEXT NOT NULL,
 patronymic  TEXT NOT NULL,
-second_name TEXT NOT NULL,
-work_time_start DATETIME2,
-work_time_end DATETIME2,
-special TEXT NOT NULL 
+second_name TEXT NOT NULL
 );
 CREATE TABLE patients(
-card_id INT PRIMARY KEY,
+card_id INTEGER PRIMARY KEY,
 full_name TEXT NOT NULL,--слитно т.к в дальшейм нужно только  целеком 
+adress TEXT NOT NULL 
 );
 CREATE TABLE rooms(
-room_id INT PRIMARY KEY CHECK (floor_num>0),
-floor_num INT CHECK (floor_num>0),
+room_id INTEGER PRIMARY KEY CHECK (floor_num>0),
+floor_num INTEGER CHECK (floor_num>0)
+);
+CREATE TABLE work_place(
 work_time_start DATETIME2,
 work_time_end DATETIME2,
-special TEXT NOT NULL
+special TEXT NOT NULL ,
+room_id INTEGER,
+doctor_id INTEGER,
+FOREIGN KEY (doctor_id) REFERENCES doctors(id),
+FOREIGN KEY (room_id) REFERENCES rooms(room_id)
 );
 CREATE TABLE appointments(
 apointment_start_time DATETIME2 PRIMARY KEY,
-apointment_time INT CHECK (apointment_time>0),--в минутах 
-doc_id INT  FOREIGN KEY(doc_id) REFERENCES doctors(id),
-card_id INT FOREIGN KEY(card_id) REFERENCES patients(card_id),
-room_id INT FOREIGN KEY(room_id) REFERENCES rooms(room_id),
-special TEXT
+apointment_time INTEGER CHECK (apointment_time>0),--в минутах 
+doc_id INTEGER, 
+card_id INTEGER,
+room_id INTEGER, 
+special TEXT,
+FOREIGN KEY (card_id) REFERENCES patients(card_id),
+FOREIGN KEY (room_id,doc_id) REFERENCES work_place(room_id,doctor_id)
 );
 
 --создание таблицы с именем books
